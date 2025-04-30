e5f4fbfd4078451848db1341c782ab52cb398713
Mehedi Hasan <hasanmehdi89@gmail.com>
2025-04-30 12:24:07 +0600

    Revert "Update 0001_detailed_time_entries.sql"
    This reverts commit 69b036f38039af366d78ccca9575c40d5462e589.

--- src/db/migrations/0001_detailed_time_entries.sql
+++ src/db/migrations/0001_detailed_time_entries.sql
@@ -1,3 +1,3 @@
--- Update time_entries table to use decimal for durationDecimal
+-- Update time_entries table to use decimal for duration_decimal
 ALTER TABLE time_entries
-ALTER COLUMN "durationDecimal" TYPE DECIMAL(10, 2);
+ALTER COLUMN duration_decimal TYPE DECIMAL(10, 2);
